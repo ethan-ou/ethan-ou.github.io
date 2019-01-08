cf1d44022ab36f1e5ab763a0057aeece06ce923c
ethan-ou <ethan-ou@users.noreply.github.com>
2019-01-08 13:04:17 +1100

    Testing Loads

--- preload.js
+++ preload.js
@@ -1,6 +1,10 @@
 window.onload = function() {
 	setTimeout(function() {
-		// preload image
+        // preload image
+        var xhr = new XMLHttpRequest();
+		xhr.open('GET', './icons/webfonts/fa-solid-900.woff2');
+		xhr.send('');
+
         new Image().src = "./icons/svg/chat-bold.svg";
         new Image().src = "./icons/svg/chat-bold-night.svg";
         new Image().src = "./icons/svg/conversation-bold.svg";
